6bc83661835e25542e1d1803ef2b9dc5d5fbdb92
Alan <alanjamestuckwood@gmail.com>
2021-11-11 01:09:10 +0000

    fix: Fixing CORS

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM node:11
 COPY . .
 RUN npm install --unsafe-perm
 run npm run-script build
-EXPOSE 5000
+EXPOSE 80
 CMD [ "node", "./dist/bundle.min.js" ]
--- src/index.ts
+++ src/index.ts
@@ -38,8 +38,8 @@ const main = async (): Promise<void> => {
   )
 
   // Start HTTP Server
-  app.listen(5000, (): void => {
-    console.log(`Server running on ${5000}`)
+  app.listen(80, (): void => {
+    console.log(`Server running on ${80}`)
   })
 }
 
